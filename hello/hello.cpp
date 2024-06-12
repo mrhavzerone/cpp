52e06937c9e0b3ef10a2b51a751b9d0610d9027d
mrhavzerone <mrhavzerone@proton.me>
2024-06-12 10:53:55 +0300

    create min-plus calculating program

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+int minus(int a,int b){
+	return a -= b;
+}
+int plus(int a, int b){
+	return a +=b;
+}
+int  mnog(int a,int b){
+	return a *=b;
+}
 int main(){
-	std::cout << "Hello World!" << std::endl;
+	int a = 1000000, b = 1;
+	std::cout << "Hello, it`s plus-minus program!" << std::endl;
+	for (int i = 1; i <= 2048; i++){
+
+	std::cout << "Result program:"<<mnog(i,i) <<std::endl;
+	}
 	return 0;
 }
