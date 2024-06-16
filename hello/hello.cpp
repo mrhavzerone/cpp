80f1ff0acc7a810f1eefd050c7ecfa818faa1460
mrhavzerone <mrhavzerone@proton.me>
2024-06-16 13:17:26 +0300

    error

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
-int minus(int a,int b){
-	return a -= b;
-}
-int plus(int a, int b){
-	return a +=b;
-}
-int  mnog(int a,int b){
-	return a *=b;
+class program()
+{
+	
+
+
+
+
+
+
+
+
+
+	int minus(int a,int b){
+		return a -= b;
+	}
+	int plus(int a, int b){
+		return a +=b;
+	}
+	int  mnog(int a,int b){
+		return a *=b;
+	}
+	int abs(int a){
+		if (a <= 0){
+			return -a;
+		}
+		else return a;
+	}
 }
 int main(){
 	int a = 1000000, b = 1;
-	std::cout << "Hello, it`s plus-minus program!" << std::endl;
-	for (int i = 1; i <= 2048; i++){
-
-	std::cout << "Result program:"<<mnog(i,i) <<std::endl;
-	}
+	std::cout << "Hello, it`s abs program!" << std::endl;
+	program.new() = prg;
+	std::cout << abs(43423);
 	return 0;
 }
